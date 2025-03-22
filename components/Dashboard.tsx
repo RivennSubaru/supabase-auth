27fe0ce36777acf8754b6bd0fe9b219d0507770a
Rivenn <zafimenasteven@gmail.com>
2025-03-22 04:22:17 +0300

    Add Dashboard and ThemeProvider components with mode toggle functionality

---
+++ components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ModeToggle } from './ModeToggle'
+
+const Dashboard = () => {
+  return (
+    <section>
+        <ModeToggle/>
+    </section>
+  )
+}
+
+export default Dashboard
